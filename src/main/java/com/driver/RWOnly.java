c4200ea14df9a59f47913c369516cf73bd8e4601
SRE-NADH <sreenadh669@gmail.com>
2023-04-12 19:18:19 +0530

    fist

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name="";
+     RWOnly(String name){
+         this.Name=name;
+     }
+
+    public String getName() { 
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
