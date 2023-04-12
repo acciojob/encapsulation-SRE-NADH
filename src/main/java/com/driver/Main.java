bc8ec39bb3b8267736be8e26bd28dbe0704a440a
SRE-NADH <sreenadh669@gmail.com>
2023-04-12 22:42:09 +0530

    check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
       RWOnly rw = new RWOnly();
+      rw.Name="sreenadh";
+      // name variable is private cnnot access
       rw.setName("gopinath");
       System.out.println(rw.getName());
     }
